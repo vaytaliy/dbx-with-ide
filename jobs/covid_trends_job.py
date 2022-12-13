5ad9f30b7b4b483fb41453044cc95d60aa1dfd25
vitalyzhidkih@gmail.com <vitalyzhidkih@gmail.com>
2022-12-13 13:26:19 +0000

    try file as notebook

--- jobs/covid_trends_job.py
+++ jobs/covid_trends_job.py
@@ -1,40 +1,43 @@
+# Databricks Notebook
 '''
 Python Spark job that imports the latest COVID-19 hospitalization data
 '''
+
+
 import sys
 import pandas as pd
 from pyspark.sql import SparkSession
 
 from covid_analysis.transforms import *
 
-def run():
-    spark = SparkSession.builder.getOrCreate()
 
-    # check if job is running in production mode
-    is_prod = len(sys.argv) >= 2 and sys.argv[1] == "--prod"
+spark = SparkSession.builder.getOrCreate()
 
-    # read from /tmp, subset for USA, pivot and fill missing values
-    df = pd.read_csv(
-        "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv")
+# check if job is running in production mode
+is_prod = len(sys.argv) >= 2 and sys.argv[1] == "--prod"
 
-    df = filter_country(df)
-    df = pivot_and_clean(df, fillna=0)
-    df = clean_spark_cols(df)
-    df = index_to_col(df, colname='date')
+# read from /tmp, subset for USA, pivot and fill missing values
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/hospitalizations/covid-hospitalizations.csv")
 
-    # Convert from Pandas to a pyspark sql DataFrame.
-    df = spark.createDataFrame(df)
+df = filter_country(df)
+df = pivot_and_clean(df, fillna=0)
+df = clean_spark_cols(df)
+df = index_to_col(df, colname='date')
 
-    # only write table in production mode
-    if is_prod:
-        # Write to Delta Lake
-        df.write.mode('overwrite').saveAsTable('covid_stats')
-        print("Covid data successfully imported.")
+# Convert from Pandas to a pyspark sql DataFrame.
+df = spark.createDataFrame(df)
 
-    # display sample data
-    if is_prod:
-        spark.sql('select * from covid_stats').show(10)
-    else:
-        df.show(10)
+# only write table in production mode
+if is_prod:
+    # Write to Delta Lake
+    df.write.mode('overwrite').saveAsTable('covid_stats')
+    print("Covid data successfully imported.")
+
+# display sample data
+if is_prod:
+    spark.sql('select * from covid_stats').show(10)
+else:
+    df.show(10)
 
    
